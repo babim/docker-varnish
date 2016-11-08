593bd05ecc753b8ffc7c925da4b2f6e4050a9fb7
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-08 21:12:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 export TERM=xterm
 
+if [ -z "`ls /etc/varnish`" ] 
+then
+	cp -R /etc-start/varnish/* /etc/varnish
+fi
+
 # Convert environment variables in the conf to fixed entries
 # http://stackoverflow.com/questions/21056450/how-to-inject-environment-variables-in-varnish-configuration
 for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_IP
